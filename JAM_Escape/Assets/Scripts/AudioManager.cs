2c14793aa0bb88ce5dd44b221c408dd54f5e99fc
rodzer <rodolphe.dupuis@epitech.eu>
2020-04-25 16:53:18 +0200

    adding audio source menu + some metas /!\

---
+++ JAM_Escape/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public Button button;
+
+    public AudioSource audioMenu;
+    bool isMuted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick() {
+        if (isMuted == false) {
+            audioMenu.Pause();
+            isMuted = true;
+        } else {
+            audioMenu.Play();
+            isMuted = false;
+        }
+    }
+
+}
