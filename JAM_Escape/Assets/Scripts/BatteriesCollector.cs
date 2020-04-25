5f21aa7944a55d31bddc4968a89d015ad7fb5041
Le Colleter Yoann <yoannlecolleter@gmail.com>
2020-04-25 18:14:43 +0200

    Adding battery collection and sound with it

---
+++ JAM_Escape/Assets/Scripts/BatteriesCollector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteriesCollector : MonoBehaviour
+{
+    public AudioSource Collect;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerControl controller = other.GetComponent<PlayerControl>();
+
+        if (controller != null)
+        {
+            Collect.Play();
+            gameObject.SetActive(false);
+        }
+    }
+}
