7afaba37cbe3ae978883e8f9c9f811ccc93c1b8b
Eliott Pal <eliott.^palueau@epitech.eu>
2020-04-26 12:07:03 +0200

    Adding moving laser

---
+++ JAM_Escape/Assets/Scripts/LaserManage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserManage : MonoBehaviour
+{
+    public float forwardSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, forwardSpeed * Time.deltaTime, 0f, null);
+        if (transform.position.y >= 16.45)
+            forwardSpeed = 0;
+    }
+}
